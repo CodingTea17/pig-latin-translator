23a80bb22ea3cee8e163c333defa36138acbfb13
Dawson Mortenson and sean slingerland <dawson@>
2017-08-16 11:54:41 -0700

    add functions to handle words that begin with qu and vowels

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,15 @@
 // BACKEND LOGIC
 var firstLetterChecker = function(word) {
-  if(word.charAt(0) === "y"){
+  if(word.charAt(0) === "y") {
 
-  } else if (word.charAt(0) === "q"){
-
+  } else if (word.charAt(0) === "q" && word.charAt(1) === "u") {
+    word = qCase(word);
+    return word;
   }
   // Uses the vowelChecker func to determine if the word begins with a vowel
-  else if (vowelChecker(word.charAt(0))){
-
+  else if (vowelChecker(word.charAt(0))) {
+    word = vowelCase(word);
+    return word;
   }
   // IF the word DOESN'T begin with a 'Y', 'Y', or 'VOWEL'. Then it must be a consonant.
   else {
@@ -15,11 +17,24 @@ var firstLetterChecker = function(word) {
   }
 };
 
+// It will handle words that start with "qu"
+var qCase = function(quWord) {
+  var quTranslated = (quWord.slice(2) + "qu");
+  return quTranslated;
+  // NOW WE NEED TO CHECK FOR VOWELS AND ADD "AY" TO THE END
+};
+
+// It will handle words that start with vowels by adding the phrase "way" to the end of them.
+var vowelCase = function(vowelWord) {
+  var vowelTranslated = vowelWord.concat("way");
+  return vowelTranslated;
+}
+
 // It loops through an array of vowels and compares them to FIRSTLETTER.
 // IF there's a match then the func returns TRUE. Default is FALSE.
 var vowelChecker = function(firstLetter) {
   vowels = ["a", "e", "i", "o", "u", "y"];
-  for(var i = 0; i < vowels.length; i++){
+  for(var i = 0; i < vowels.length; i++) {
     if(vowels[i] === firstLetter){
       return true;
     }
@@ -38,6 +53,7 @@ $(document).ready(function() {
     // Splits the sentence into an array of words
     var arrayOfEnglishWords = userEnglish.split(" ");
 
-    arrayOfEnglishWords.forEach(firstLetterChecker);
+    var translatedArray = arrayOfEnglishWords.map(firstLetterChecker);
+    alert(translatedArray);
   });
 });
